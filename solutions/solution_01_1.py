e1865dee64ba1c0f88971375a9857f3e680ac4c5
Joseph Hobbs <joseph.hobbs@gmail.com>
2024-12-01 16:13:20 -0500

    solutions for day 1

---
+++ solutions/solution_01_1.py
@@ -0,0 +1,32 @@
+################################################################################
+# solution_01_1.py
+################################################################################
+
+def solve(input_filename: str, input_data: list, use_debug: bool):
+
+    # Initlaize two lists; one for each column.  Fill them and then sort
+    # each list individually so values are in order.
+
+    list1 = []
+    list2 = []
+    for input_line in input_data:
+        value1, value2 = input_line.split()
+        list1.append(int(value1))
+        list2.append(int(value2))
+
+    list1.sort()
+    list2.sort()
+
+    # Calculate the result by subtracting values between the lists. Since
+    # there is no guarantee on which is higher, take the absolute value to
+    # eliminate any negatives.
+
+    result = 0
+    for i in range(len(list1)):
+        result += abs(list1[i] - list2[i])
+
+    print(result)
+
+################################################################################
+# END
+################################################################################
---
+++ solutions/solution_01_2.py
@@ -0,0 +1,30 @@
+################################################################################
+# solution_01_2.py
+################################################################################
+
+def solve(input_filename: str, input_data: list, use_debug: bool):
+
+    # Initlaize two lists and fill them based on the values in each column.
+
+    list1 = []
+    list2 = []
+    for input_line in input_data:
+        value1, value2 = input_line.split()
+        list1.append(int(value1))
+        list2.append(int(value2))
+
+    # For each value in the first list, count the number of times it occurs
+    # within the second list. Multiply the value by the number of hits (in the)
+    # 2nd list and add it to the result.
+
+    result = 0
+
+    for i in list1:
+        hits = list2.count(i)
+        result += (i * hits)
+
+    print(result)
+
+################################################################################
+# END
+################################################################################
